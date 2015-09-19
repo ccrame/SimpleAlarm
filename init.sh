b67b76e7e4637574d10fcdebfad9d5d738b8ffb5
Charles Crame <ccrame@gmail.com>
2015-09-18 20:33:00 -0700

    fixed dependency issue by using cdn

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 npm install
-bower install
 node ./server.js
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var port = process.env.PORT || 4000;
-
 var app = express();
 
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/', express.static(__dirname + '/client'));
 app.listen(port);

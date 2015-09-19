08857712408787a0a3740c328e1f9c439a26c2fa
Charles Crame <ccrame@gmail.com>
2015-09-18 20:09:39 -0700

    fixed file path error

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var port = process.env.PORT || 4000;
 
 var app = express();
 
-app.get('/bower_components/*',function(req,res){
-  res.sendFile(__dirname + req.originalUrl);
-});
-
-app.use(express.static(__dirname + '/client'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/', express.static(__dirname + '/client'));
 app.listen(port);
